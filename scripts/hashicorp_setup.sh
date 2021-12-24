4e5ef6d09e82298c87fd6334a91246e9e46ed4cb
Armand-Cezar Mathe <me@cezarmathe.com>
2021-12-24 18:28:39 +0200

    Add script for setting up the hashicorp apt repo

---
+++ scripts/hashicorp_setup.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+# hashicluster-iac - scripts/hashicorp_setup.sh
+#
+# This script installs the HashiCorp repository.
+
+set -euxo pipefail
+
+# select architecture for this machine
+ARCH=""
+if [[ "$(uname -m)" == "x86_64" ]]; then
+    ARCH="amd64"
+elif [[ "$(uname -m)" == "amd64" ]]; then
+    ARCH="amd64"
+elif [[ "$(uname -m)" == "aarch64" ]]; then
+    ARCH="arm64"
+elif [[ "$(uname -m)" == "arm64" ]]; then
+    ARCH="arm64"
+else
+    printf "%s\n" "Unsupported architecture: $(uname -m)"
+    exit 1
+fi
+
+# set up hashicorp repository
+
+curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
+
+sudo apt-add-repository "deb [arch=${ARCH}] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
